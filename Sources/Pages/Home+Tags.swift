9cc80f0dd42da6416ff7aa48694c2befdfedecfe
Mark Stamer <markstamer@gmail.com>
2024-07-01 22:37:50 +0200

    Change badge color

--- Sources/Pages/Home+Tags.swift
+++ Sources/Pages/Home+Tags.swift
@@ -14,14 +14,14 @@ extension Home {
             Card.badgeCard(
                 title: "Knowledge",
                 badges: ["Swift", "SwiftUI", "UIKit", "async/await", "Combine", "MapKit", "CoreLocation", "SwiftPM", "..."],
-                role: .warning
+                role: .success
             )
             .width(4)
 
             Card.badgeCard(
                 title: "Favorite Creators",
                 badges: ["Majid Jabrayilov", "Paul Hudson", "Sarun W.", "Chris Eidhof", "Javier (swiftui-lab)", "John Sundell", "Soroush Khanlou", "Janum Trivedi"],
-                role: .secondary
+                role: .warning
             )
             .width(5)
         }
