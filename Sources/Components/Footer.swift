3cb46ce145b82743318b1e35c87fffec84accb8b
Mark Stamer <markstamer@gmail.com>
2024-07-04 00:09:53 +0200

    Add footer

--- Sources/Components/Footer.swift
+++ Sources/Components/Footer.swift
@@ -1,8 +1,39 @@
-//
-//  File.swift
-//  
-//
-//  Created by Mark Stamer on 03.07.24.
-//
-
 import Foundation
+import Ignite
+
+struct Footer: Component {
+    public func body(context: PublishingContext) -> [any PageElement] {
+        Group {
+            Text {
+                Image(systemName: "c-circle")
+
+                "2024 Mark Stamer"
+
+                Image(systemName: "dot")
+
+                Link("Legal notice", target: Imprint())
+                    .role(.secondary)
+                    .linkStyle(.hover)
+
+                Image(systemName: "dot")
+
+                " Created with "
+
+                Link("Ignite", target: URL("https://github.com/twostraws/Ignite"))
+                    .role(.secondary)
+                    .linkStyle(.hover)
+
+                " using "
+
+                Image(decorative: "images/swift_logo_white.svg")
+                    .resizable()
+                    .frame(height: "1.4em")
+                    .margin(.bottom, -2)
+                    .verticalAlignment(.textBottom)
+            }
+            .foregroundStyle(.tertiary)
+        }
+        .horizontalAlignment(.center)
+        .margin(.top, .extraLarge)
+    }
+}
