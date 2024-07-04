a6841e31b155717738cb22cd1f5d1f4ab5db7529
Mark Stamer <markstamer@gmail.com>
2024-07-04 12:41:05 +0200

    Move imprint from markdown to text elements

--- Sources/Pages/Imprint.swift
+++ Sources/Pages/Imprint.swift
@@ -4,51 +4,53 @@ struct Imprint: StaticPage {
     var title: String = "Imprint"
 
     func body(context: PublishingContext) async -> [any BlockElement] {
-        Section {
-            Text(markdown: """
-            # -
-            # Imprint
+        Group {
+            Text("Imprint").font(.title1)
 
-            ### Information pursuant to § 5 TMG:
+            Text("Information pursuant to § 5 TMG:").font(.title4)
 
-            **Dr.-Ing. Mark Stamer**
+            Text {
+                Text("Dr.-Ing. Mark Stamer")
+                Text("Himmelstraße 33")
+                Text("22299 Hamburg")
+                Text("Germany")
+            }
 
-            Himmelstraße 33
+            Text("Contact:").font(.title4)
 
-            22299 Hamburg
+            Group {
+                Text("E-Mail: markstamer@gmail.com")
+                Text("Website: markstamer.github.io")
+            }
 
-            Germany
+            Text("VAT ID:").font(.title4)
 
-            #### Contact:
+            Text("VAT identification number according to § 27 a Value Added Tax Act: DE354159226")
 
-            E-Mail: markstamer@gmail.com
+            Text("Disclaimer").font(.title4)
 
-            Website: markstamer.github.io
+            Text("Liability for Content").font(.title5)
 
-            #### VAT ID:
+            Group {
+                Text("As a service provider, we are responsible for our own content on these pages in accordance with § 7 Abs.1 TMG under general laws. According to §§ 8 to 10 TMG, we are not obliged to monitor transmitted or stored external information or to investigate circumstances that indicate illegal activity.")
 
-            VAT identification number according to § 27 a Value Added Tax Act: DE354159226
+                Text("Obligations to remove or block the use of information under general laws remain unaffected. However, liability in this regard is only possible from the time of knowledge of a specific infringement. Upon notification of such violations, we will remove this content immediately.")
+            }
+            Text("Liability for Links").font(.title5)
 
-            ### Disclaimer
+            Group {
+                Text("Our offer contains links to external websites of third parties, on whose contents we have no influence. Therefore, we cannot assume any liability for these external contents. The respective provider or operator of the pages is always responsible for the contents of the linked pages. The linked pages were checked for possible legal violations at the time of linking. Illegal contents were not recognizable at the time of linking.")
 
-            ##### Liability for Content
+                Text("A permanent content control of the linked pages is, however, not reasonable without concrete evidence of a violation of law. Upon notification of violations, we will remove such links immediately.")
+            }
 
-            As a service provider, we are responsible for our own content on these pages in accordance with § 7 Abs.1 TMG under general laws. According to §§ 8 to 10 TMG, we are not obliged to monitor transmitted or stored external information or to investigate circumstances that indicate illegal activity.
+            Text("Copyright").font(.title5)
 
-            Obligations to remove or block the use of information under general laws remain unaffected. However, liability in this regard is only possible from the time of knowledge of a specific infringement. Upon notification of such violations, we will remove this content immediately.
-            
-            ##### Liability for Links
+            Group {
+                Text("The contents and works created by the site operators on these pages are subject to German copyright law. The duplication, processing, distribution, and any kind of exploitation outside the limits of copyright law require the written consent of the respective author or creator. Downloads and copies of this site are only permitted for private, non-commercial use.")
 
-            Our offer contains links to external websites of third parties, on whose contents we have no influence. Therefore, we cannot assume any liability for these external contents. The respective provider or operator of the pages is always responsible for the contents of the linked pages. The linked pages were checked for possible legal violations at the time of linking. Illegal contents were not recognizable at the time of linking.
-
-            A permanent content control of the linked pages is, however, not reasonable without concrete evidence of a violation of law. Upon notification of violations, we will remove such links immediately.
-
-            ##### Copyright
-
-            The contents and works created by the site operators on these pages are subject to German copyright law. The duplication, processing, distribution, and any kind of exploitation outside the limits of copyright law require the written consent of the respective author or creator. Downloads and copies of this site are only permitted for private, non-commercial use.
-
-            Insofar as the content on this site was not created by the operator, the copyrights of third parties are respected. In particular, third-party content is marked as such. Should you nevertheless become aware of a copyright infringement, please inform us accordingly. Upon notification of violations, we will remove such content immediately.
-            """)
+                Text("Insofar as the content on this site was not created by the operator, the copyrights of third parties are respected. In particular, third-party content is marked as such. Should you nevertheless become aware of a copyright infringement, please inform us accordingly. Upon notification of violations, we will remove such content immediately.")
+            }
         }
         .margin(.vertical, .extraLarge)
         .frame(maxWidth: .containerWidth(.large))
