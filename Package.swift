519569790aa169be74d41e2424b68b3ec5b9e706
Mark Stamer <markstamer@gmail.com>
2024-09-17 10:48:01 +0200

    Use MyIngite as local package folder

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "PortfolioSite",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(name: "Ignite", path: "../Ignite")
+        .package(name: "Ignite", path: "../MyIgnite")
     ],
     targets: [
         .executableTarget(
